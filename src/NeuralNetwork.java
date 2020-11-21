dab6011f644186657f1223124347df5b54a60b3a
Txmyx <53293648+Toom-boop@users.noreply.github.com>
2020-11-21 17:20:14 +0100

    Update NeuralNetwork.java

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -1,50 +1,52 @@
-package userfriendly;
-
 public class NeuralNetwork {
-	private Layer[] layers;
-	private double[][][] outputLayers;
-	private double learningRate;
-	
-	public NeuralNetwork(Layer... layers) {
-		this(0.1, layers);
-	}
-	
-	public NeuralNetwork(double leariningRate, Layer... layers) {
-		if(layers.length == 0) throw new NullPointerException();
-		this.layers = layers;
-		outputLayers = new double[this.layers.length][0][0];
-	}
+	private final Layer[] layers;
+    private double[][][] outputLayers;
+    private final double learningRate;
 
-	public double[][] run(double[][] input) {
-		for(int i = 0; i < layers.length; i++) {
-			if(i == 0) {
-				outputLayers[i] = NNMath.apply(NNMath.matrixMultiply(input, layers[i].weights()), layers[i].activationFunction());
-			}else {
-				outputLayers[i] = NNMath.apply(NNMath.matrixMultiply(outputLayers[i-1], layers[i].weights()), layers[i].activationFunction());
-			}
-		}
-		return outputLayers[outputLayers.length - 1];
-	}
-	
-	public void train(double[][] inputs, double[][] outputs, int iterations) {
-		for(int l = 0; l < iterations; l++) {
-			run(inputs);
-			double[][][] deltaLayers = new double[layers.length][0][0];
-			for(int i = layers.length; i > 0; i--) {
-				if(i == layers.length) {
-		            deltaLayers[i] = NNMath.scalarMultiply(NNMath.matrixSubtract(outputs, outputLayers[i]), NNMath.apply(outputLayers[i], layers[i].activationFunctionDerivative()));
-				}else {
-		            deltaLayers[i] = NNMath.scalarMultiply(NNMath.matrixMultiply(deltaLayers[i+1], NNMath.matrixTranspose(layers[i+1].weights())), NNMath.apply(outputLayers[i], layers[i].activationFunctionDerivative()));
-				}
-				if(i == 0) {
-					layers[i].adjustWeights(NNMath.apply(NNMath.matrixMultiply(NNMath.matrixTranspose(inputs), deltaLayers[i]), (x) -> learningRate * x));
-				}else {
-					layers[i].adjustWeights(NNMath.apply(NNMath.matrixMultiply(NNMath.matrixTranspose(outputLayers[i+1]), deltaLayers[i]), (x) -> learningRate * x));
-				}
-			}
-			if(l % (iterations/10) == 0){
-                System.out.println("Iteration " + l + "/" + iterations);
+    public NeuralNetwork(NeuronLayer... layers) {
+        this(0.1, layers);
+    }
+
+    public NeuralNetwork(double learningRate, NeuronLayer... layers) {
+        this.layers = layers;
+        this.learningRate = learningRate;
+        outputLayers = new double[layers.length][0][0];
+    }
+    
+    public double[][] run(double[][] inputs) {
+    	for(int i = 0; i<outputLayers.length; i++) {
+    		if(i == 0) {
+    			outputLayers[i] = NNMath.apply(NNMath.matrixMultiply(inputs, layers[i].weights()), layers[i].activationFunction());
+    		}else {
+    			outputLayers[i] = NNMath.apply(NNMath.matrixMultiply(outputLayers[i-1], layers[i].weights()), layers[i].activationFunction());
+    		}
+        }
+        return outputLayers[outputLayers.length - 1];
+    }
+
+    public void train(double[][] inputs, double[][] outputs, int numberOfTrainingIterations) {
+        for (int i = 0; i < numberOfTrainingIterations; ++i) {
+            run(inputs);
+            
+            double[][][] deltaLayers = new double[layers.length][0][0];
+            
+            for(int l = layers.length - 1; l>0; l--) {          	
+            	if(l == layers.length - 1) {
+                    deltaLayers[l] = NNMath.scalarMultiply(NNMath.matrixSubtract(outputs, outputLayers[l]), NNMath.apply(outputLayers[l], layers[l].activationFunctionDerivative()));
+            	}else {
+                    deltaLayers[l] = NNMath.scalarMultiply(NNMath.matrixMultiply(deltaLayers[l+1], NNMath.matrixTranspose(layers[l+1].weights())), NNMath.apply(outputLayers[l], layers[l].activationFunctionDerivative()));
+            	}
+            	
+            	if(l == 0) {
+                    layers[l].adjustWeights(NNMath.apply(NNMath.matrixMultiply(NNMath.matrixTranspose(inputs), deltaLayers[l]), (x) -> learningRate * x));
+            	}else {
+                    layers[l].adjustWeights(NNMath.apply(NNMath.matrixMultiply(NNMath.matrixTranspose(outputLayers[l-1]), deltaLayers[l]), (x) -> learningRate * x));
+            	}
             }
-		}
-	}
+
+            if(i % (numberOfTrainingIterations/10) == 0){
+                System.out.println("Training iteration " + i + " of " + numberOfTrainingIterations);
+            }
+        }
+    }
 }
