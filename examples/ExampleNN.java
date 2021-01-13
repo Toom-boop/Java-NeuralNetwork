c487c5a02a73533ccab562b994062b23bb22d2e4
Txmyx <53293648+Toom-boop@users.noreply.github.com>
2021-01-13 16:46:21 +0100

    Update ExampleNN.java

--- examples/ExampleNN.java
+++ examples/ExampleNN.java
@@ -1,28 +1,40 @@
 public class ExampleNN {
-	public static void main(String[] args) {
-		NeuralNetwork net = new NeuralNetwork(new NeuronLayer(3, 3), new NeuronLayer(3, 3), new NeuronLayer(3, 1));
+	public static void main(String[] args) {	
+		Tensor inputs = new Tensor();
+		inputs.set(0, 0, 1);
+		inputs.set(0, 1, 1);
+		inputs.set(0, 2, 1);
 		
-		double[][] inputs = new double[][] {{1, 0, 1}, {0, 1, 0}, {1, 1, 1}, {1, 1, 0}, {0, 1, 1}};
-		double[][] outputs = new double[][] {{1}, {0}, {1}, {1}, {0}};
+		inputs.set(1, 0, 1);
+		inputs.set(1, 1, 1);
+		inputs.set(1, 2, 0);
+		
+		inputs.set(2, 0, 0);
+		inputs.set(2, 1, 1);
+		inputs.set(2, 2, 1);
+		
+		inputs.set(3, 0, 0);
+		inputs.set(3, 1, 1);
+		inputs.set(3, 2, 0);
+		
+		Tensor outputs = new Tensor();
+		outputs.set(0, 0, 1);
+		outputs.set(0, 1, 1);
+		outputs.set(0, 2, 0);
+		outputs.set(0, 3, 0);
+
+		Layer layer1 = new NeuronLayer(3, 2);
+		Layer layer2 = new NeuronLayer(2, 1);
+		
+		NeuralNetwork net = new NeuralNetwork(0.2, layer1, layer2);
 		
 		net.train(inputs, outputs, 10000);
 		
-		print(net.run(new double[][] {{1, 0, 1}}));
-	}
-	
-	public static void print(double[][] a) {
-		for(int i = 0; i<a.length; i++) {
-			for(int l = 0; l<a[0].length; l++) {
-				if(a[i][l] <= 0) {
-					System.out.print(".");
-				}else if(a[i][l] == 1) {
-					System.out.print("X");
-				}else {
-					System.out.print(a[i][l]);
-				}
-			}
-			System.out.println();
-		}
-		System.out.println();
+		Tensor input = new Tensor();
+		input.set(0, 0, 0);
+		input.set(0, 1, 1);
+		input.set(0, 2, 0);
+		
+		System.out.print(String.format("%.5g%n", net.run(input).toDouble()));
 	}
 }
